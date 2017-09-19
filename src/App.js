08874c76fae535730d8d355a8a30e2afc3503335
Adam Abonyi <adam.abonyi@summitmedia.com>
2017-09-19 13:09:30 +0200

    Change to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import logo from './logo.svg';
 import './App.css';
+import Tralala from './Tralala';
 
 class App extends Component {
   render() {
@@ -16,8 +17,10 @@ class App extends Component {
         <p className="App-intro">
           {/* To get started, edit <code>src/App.js</code> and save to reload. */}
           Kiss Adam to continue. Also i have added a button that does nothing :-)
+          Level 1
         </p>
         <RaisedButton label="Material UI" />
+        <Tralala name="adam"/>
       </div>
       </MuiThemeProvider>
     );
@@ -25,3 +28,7 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
---
+++ src/Tralala.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Tralala extends Component {
+  render() {
+    return (
+      <div>{this.props.title} {this.props.name} je krásný kluk</div>
+    )
+  }
+}
+
+export default Tralala;
+
